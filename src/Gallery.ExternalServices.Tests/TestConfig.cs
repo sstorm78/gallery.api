21b3eff074b85e2f735c00526b9e6ccf3019f53e
sergey storm <westernstorm@gmail.com>
2019-11-15 20:07:26 +0000

    removed some tests

--- src/Gallery.ExternalServices.Tests/TestConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Gallery.ExternalServices.Tests
-{
-    public class TestConfig : Models.IConfig
-    {
-        public string ExternalServicesTypiCodeUrl { get; set; }
-
-        public int ExternalServicesTypiCodeTimeoutSeconds { get; set; }
-    }
-}
